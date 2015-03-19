586ea42090a96629795f9c102918be30185d9baa
Kadriye Taylan <kdryetyln@gmail.com>
2015-03-20 01:22:03 +0200

    Create while.c

---
+++ while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x ;
+    x = 10;
+    while (x>=1)
+    {
+        printf(" %d \n", x) ;
+        x = x - 1;
+    }
+    return 0 ;
+}
