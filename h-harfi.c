5e44e4b599bf9107d8e9df65af6dcdf2bf83707e
Kadriye Taylan <kdryetyln@gmail.com>
2015-03-22 21:16:26 +0200

    Create h-harfi.c

---
+++ h-harfi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,k;
+
+    for(i=0;i<=0;i++)
+    {
+        for(j=0;j<=3;j++)
+        {
+            printf("*\t*\n");
+        }
+        printf("*********\n");
+        for(k=0;k<=3;k++)
+        {
+            printf("*\t*\n");
+        }
+    }
+
+    return 0;
+}
